d4d53e4c4d2fcb090476c5054ad2d9c063bdc142
Alex Smith <alex.smith@suntecktts.com>
2022-03-13 12:08:38 -0600

    Implement Shopping Cart Classes
    Create a builder class that will take a list of items and return a
    ShoppingCart object populated with the number of each type of item.

--- main.rb
+++ main.rb
@@ -3,8 +3,14 @@
 require File.join(File.dirname(__FILE__), 'services/shopping_cart_builder')
 require File.join(File.dirname(__FILE__), 'services/shopping_cart_price_calculator')
 
-items = ['milk','milk','bread','banana','bread','bread','bread','milk','apple']
+items = ['milk','milk','bread','banana','bread','bread','bread','milk','apple','']
 
 shopping_cart = ShoppingCartBuilder.new.build_cart(items: items)
 
-raise 'Oh no!' if ShoppingCartPriceCalculator.new.calculate_total(shopping_cart: shopping_cart) != '$19.02'
+raise 'Wrong count milk!' if shopping_cart.item_count(item_name: 'milk') != 3
+raise 'Wrong count bread!' if shopping_cart.item_count(item_name: 'bread') != 4
+raise 'Wrong count apple!' if shopping_cart.item_count(item_name: 'apple') != 1
+raise 'Wrong count banana!' if shopping_cart.item_count(item_name: 'banana') != 1
+raise 'Wrong count empty!' if shopping_cart.item_count(item_name: '') != 0
+
+raise 'This should fail!' if ShoppingCartPriceCalculator.new.calculate_total(shopping_cart: shopping_cart) != '$19.02'
--- services/shopping_cart_builder.rb
+++ services/shopping_cart_builder.rb
@@ -2,6 +2,14 @@ require File.join(File.dirname(__FILE__), '../models/shopping_cart')
 
 class ShoppingCartBuilder
   def build_cart(items: [])
-    ShoppingCart.new
+    cart = ShoppingCart.new
+
+    items.each do |item_name|
+      next if item_name.empty?
+
+      cart.add_item(item_name: item_name)
+    end
+
+    cart
   end
 end
